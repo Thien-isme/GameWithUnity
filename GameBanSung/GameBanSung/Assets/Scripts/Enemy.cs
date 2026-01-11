8fde5bf9a0c4700edd08926bd85f6af517db7839
Thien-isme <thienisme2004@gmail.com>
2026-01-12 00:17:47 +0700

    enemy eliminate player

---
+++ GameBanSung/GameBanSung/Assets/Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float moveSpeed = 0.5f;
+    private Transform playerTranform;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        if (player == null)
+        {
+            player = FindObjectOfType<GameObject>();
+        }
+
+        if (player != null)
+        {
+            playerTranform = player.transform;
+        }
+        else
+        {
+            Debug.LogError("Player object not found in the scene.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTranform != null)
+        {
+            Vector3 direction = (playerTranform.position - transform.position).normalized;
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.gameObject.SetActive(false);
+        }
+
+        if (collision.CompareTag("Bullet"))
+        {
+            Destroy(gameObject);
+            collision.gameObject.SetActive(false);
+        }
+
+    }
+}
---
+++ GameBanSung/GameBanSung/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float spawnRate = 2.0f;
+    public float spawnRadius = 6f;
+    private float spawnTimer = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer >= spawnRate)
+        {
+            SpawnEnemy();
+            spawnTimer = 0f;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, spawnRadius);
+    }
+
+    void SpawnEnemy()
+    {
+        // Điểm ngẫu nhiên TRÊN mép hình tròn bán kính spawnRadius
+        Vector2 edgePos = Random.insideUnitCircle.normalized * spawnRadius;
+
+        // Game 2D: mặt phẳng X–Y
+        Vector3 spawnPos = new Vector3(edgePos.x, edgePos.y, 0f) + transform.position;
+
+        Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+    }
+
+
+
+}
